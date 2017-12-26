ce78d9db398ba061d80d0c3aeff6329da88a837d
Kirill Riazanov <riazanovkiril99@gmail.com>
2017-12-26 17:31:44 +0300

    Изменено имя класса с "Main" на "ParallelCopying"

--- src/ru/rks/copyfiles/ParallelCopying.java
+++ src/ru/rks/copyfiles/ParallelCopying.java
@@ -1,6 +1,6 @@
 package ru.rks.copyfiles;
 
-public class Main {
+public class ParallelCopying {
     public static void main(String[] args) {
         CopyFile thread1 = new CopyFile("D:\\Work\\Threads\\src\\ru\\rks\\copyfiles\\Document1.txt");
         CopyFile thread2 = new CopyFile("D:\\Work\\Threads\\src\\ru\\rks\\copyfiles\\Document2.txt");
