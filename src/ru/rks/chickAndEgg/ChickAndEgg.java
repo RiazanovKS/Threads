b03bd9f8eb9c6e184a79b1ff555a6d0363282677
Kirill Riazanov <riazanovkiril99@gmail.com>
2017-12-29 16:52:17 +0300

    Добавлен проект "Копирование файлов"

---
+++ src/ru/rks/chickAndEgg/ChickAndEgg.java
@@ -0,0 +1,32 @@
+package ru.rks.chickAndEgg;
+
+public class ChickAndEgg {
+    static EggVoice mAnotherOpinion;
+
+    public static void main(String[] args) {
+        mAnotherOpinion = new EggVoice();
+        System.out.println("Спор начат...");
+        mAnotherOpinion.start();
+
+        for (int i = 0; i < 5; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+
+            System.out.println("курица!");
+        }
+        if (mAnotherOpinion.isAlive()) {
+            try {
+                mAnotherOpinion.join();
+            } catch (InterruptedException e) {
+            }
+
+            System.out.println("Первым появилось яйцо!");
+        } else {
+            System.out.println("Первой появилась курица!");
+        }
+        System.out.println("Спор закончен!");
+    }
+}
+
